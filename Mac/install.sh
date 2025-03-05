2d0943da02b7af84af56c83c71426d14cb92405a
tivnantu <tivnantu@tencent.com>
2025-03-05 12:35:34 +0800

    feat: 增加mac重装快捷安装脚本

---
+++ Mac/install.sh
@@ -0,0 +1,46 @@
+brew tap brewforge/chinese
+brew tap brewforge/extras
+
+brew install alt-tab \
+   android-file-transfer \
+   bat \
+   bat-extras \
+   clash-verge-rev \
+   fd \
+   firefox \
+   firefox@developer-edition \
+   firefox@nightly \
+   font-jetbrains-mono-nerd-font \
+   forgit \
+   fzf \
+   git \
+   git-delta \
+   git-lfs \
+   google-chrome \
+   i4tools \
+   iterm2 \
+   jordanbaird-ice \
+   klogg \
+   lazygit \
+   localsend \
+   lookin \
+   maccy \
+   monitorcontrol \
+   mos \
+   nomachine \
+   openinterminal \
+   pipenv \
+   qq \
+   qqmusic \
+   rectangle \
+   ripgrep \
+   sensiblesidebuttons \
+   snipaste \
+   sourcetree \
+   squirrel \
+   tencent-lemon \
+   utools \
+   visual-studio-code \
+   wechat \
+   wechatwork \
+   xcodes
